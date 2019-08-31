cfeda9bc8726d6ecad674fb958e55793df7adf8c
Brice Friha <brice.friha@aol.fr>
2019-08-31 13:40:56 +0200

    Handling x:Static

---
+++ Phoneword/Phoneword/SharedResources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public static class SharedResources
+    {
+        // New color
+        public static Color ButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+        public static Color ButtonTextColor
+        {
+            get { return Color.White; }
+        }
+    }
+}
